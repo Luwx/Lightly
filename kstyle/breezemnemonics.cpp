8e543bd61780bc80dde748253542cc37d0223d60
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-07-28 21:28:59 +0200

    added mnemonics handling

---
+++ kstyle/breezemnemonics.cpp
@@ -0,0 +1,98 @@
+//////////////////////////////////////////////////////////////////////////////
+// breezemnemonics.cpp
+// enable/disable mnemonics display
+// -------------------
+//
+// Copyright (C) 2011 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License version 2 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with this library; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+//////////////////////////////////////////////////////////////////////////////
+
+#include "breezemnemonics.h"
+
+#include <QKeyEvent>
+#include <QWidget>
+
+namespace Breeze
+{
+
+    //____________________________________________________
+    void Mnemonics::setMode( int mode )
+    {
+
+        switch( mode )
+        {
+            case StyleConfigData::MN_NEVER:
+            qApp->removeEventFilter( this );
+            setEnabled( false );
+            break;
+
+            default:
+            case StyleConfigData::MN_ALWAYS:
+            qApp->removeEventFilter( this );
+            setEnabled( true );
+            break;
+
+            case StyleConfigData::MN_AUTO:
+            qApp->removeEventFilter( this );
+            qApp->installEventFilter( this );
+            setEnabled( false );
+            break;
+
+        }
+
+        return;
+
+    }
+
+    //____________________________________________________
+    bool Mnemonics::eventFilter( QObject*, QEvent* event )
+    {
+
+        switch( event->type() )
+        {
+
+            case QEvent::KeyPress:
+            if( static_cast<QKeyEvent*>(event)->key() == Qt::Key_Alt )
+            { setEnabled( true ); }
+            break;
+
+            case QEvent::KeyRelease:
+            if( static_cast<QKeyEvent*>(event)->key() == Qt::Key_Alt )
+            { setEnabled( false ); }
+            break;
+
+            default: break;
+
+        }
+
+        return false;
+
+    }
+
+    //____________________________________________________
+    void Mnemonics::setEnabled( bool value )
+    {
+        if( _enabled == value ) return;
+
+        _enabled = value;
+
+        // update all top level widgets
+        foreach( QWidget* widget, qApp->topLevelWidgets() )
+        { widget->update(); }
+
+    }
+
+}
---
+++ kstyle/breezemnemonics.h
@@ -0,0 +1,74 @@
+#ifndef breezemnemonics_h
+#define breezemnemonics_h
+
+//////////////////////////////////////////////////////////////////////////////
+// breezemnemonics.h
+// enable/disable mnemonics display
+// -------------------
+//
+// Copyright (C) 2011 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License version 2 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with this library; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+//////////////////////////////////////////////////////////////////////////////
+
+#include <QEvent>
+#include <QObject>
+#include <QApplication>
+
+#include "breezestyleconfigdata.h"
+
+namespace Breeze
+{
+
+    class Mnemonics: public QObject
+    {
+
+        public:
+
+        //! constructor
+        explicit Mnemonics( QObject* parent ):
+            QObject( parent ),
+            _enabled( true )
+            {}
+
+        //! destructor
+        virtual ~Mnemonics( void )
+        {}
+
+        //! set mode
+        void setMode( int );
+
+        //! event filter
+        virtual bool eventFilter( QObject*, QEvent* );
+
+        //! true if mnemonics are enabled
+        const bool& enabled( void ) const
+        { return _enabled; }
+
+        protected:
+
+        //! set enable state
+        void setEnabled( bool );
+
+        private:
+
+        //! enable state
+        bool _enabled;
+
+    };
+
+}
+
+#endif
