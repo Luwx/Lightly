ab97c76396f3f03dd94762101ffd66990c34bac7
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-08-11 18:27:28 +0200

    changed license

--- kstyle/breezehelper.cpp
+++ kstyle/breezehelper.cpp
@@ -1,24 +1,24 @@
-/*
- * Copyright 2009-2010 Hugo Pereira Da Costa <hugo.pereira@free.fr>
- * Copyright 2008 Long Huynh Huu <long.upcase@googlemail.com>
- * Copyright 2007 Matthew Woehlke <mw_triad@users.sourceforge.net>
- * Copyright 2007 Casper Boemann <cbr@boemann.dk>
- * Copyright 2007 Fredrik H?glund <fredrik@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License version 2 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- */
+//////////////////////////////////////////////////////////////////////////////
+// breezestyle.cpp
+// Breeze widget style for KDE Frameworks
+// -------------------
+//
+// Copyright (C) 2014 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License version 2 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with this library; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+//////////////////////////////////////////////////////////////////////////////
 
 #include "breezehelper.h"
 
