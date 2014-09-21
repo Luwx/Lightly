9ba6a64f88989f29fbb9b89e65dfae3dccf774a1
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-09-21 20:05:48 +0200

    removed duplicated license

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -17,28 +17,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  *************************************************************************/
 
-//////////////////////////////////////////////////////////////////////////////
-// breezestyle.cpp
-// Breeze widget style for KDE Frameworks
-// -------------------
-//
-// Copyright (C) 2014 Hugo Pereira Da Costa <hugo.pereira@free.fr>
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License version 2 as published by the Free Software Foundation.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with this library; see the file COPYING.LIB.  If not, write to
-// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-// Boston, MA 02110-1301, USA.
-//////////////////////////////////////////////////////////////////////////////
-
 #include "breezestyle.h"
 #include "breezestyle.moc"
 
