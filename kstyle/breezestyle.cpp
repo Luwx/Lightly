997b2c4218e27c0957b075066a9dd7d36e41e468
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-09-30 19:10:03 +0200

    Changed flat toggled button background button to grey, on request from Andrew

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -3874,7 +3874,7 @@ namespace Breeze
         {
 
             QPalette::ColorRole textRole( QPalette::ButtonText );
-            if( flat ) textRole = (sunken&&!mouseOver) ? QPalette::HighlightedText: QPalette::WindowText;
+            if( flat ) textRole = (hasFocus&&sunken&&!mouseOver) ? QPalette::HighlightedText: QPalette::WindowText;
             else if( hasFocus&&!mouseOver ) textRole = QPalette::HighlightedText;
 
             painter->setFont(toolButtonOption->font);
