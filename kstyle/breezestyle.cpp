b3ddf362ac2355d0973d095f3d1907a2411d74f6
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-10-01 15:28:51 +0200

    do not remove pushbutton margins from contentsRect BUG: 339508

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -3697,7 +3697,7 @@ namespace Breeze
             // render
             _helper->renderArrow( painter, arrowRect, arrowColor, ArrowDown );
 
-        } else contentsRect = insideMargin( contentsRect, Metrics::Button_MarginWidth, 0 );
+        }
 
         // icon size
         QSize iconSize( buttonOption->iconSize );
