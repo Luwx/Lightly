9280f25d74eb834462a8c44242e7f2145ac5bb76
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-12-11 14:52:35 +0100

    added copyright

--- windec/kdecoration2/breezedecoration.h
+++ windec/kdecoration2/breezedecoration.h
@@ -3,6 +3,7 @@
 
 /*
  * Copyright 2014  Martin Gräßlin <mgraesslin@kde.org>
+ * Copyright 2014  Hugo Pereira Da Costa <hugo.pereira@free.fr>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
