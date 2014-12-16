d43ff9aab547faa209bae2af72c09ed0551ac035
Lukáš Tinkl <lukas@kde.org>
2014-12-16 19:23:52 +0100

    add Messages.sh for the breeze decoration

---
+++ kdecoration/Messages.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/breeze_kwin_deco.pot
+rm -f rc.cpp
