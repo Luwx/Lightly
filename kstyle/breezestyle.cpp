26e3c21757651c7f96749e5ec327730ec7fbd11d
David Edmundson <kde@davidedmundson.co.uk>
2015-02-10 17:21:20 +0100

    Avoid comparing unintialised variable if BREEZE_HAVE_KSTYLE is not set
    if BREEZE_HAVE_KSTYLE is not set CE_CapacityBar remains uninitialised
    (random value). It should therefore not be used in any comparisons.
    
    For KDE4, BREEZE_HAVE_KSTYLE will always be false regardless so that
    behaviour remains the same

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -807,7 +807,7 @@ namespace Breeze
 
         StyleControl fcn( nullptr );
 
-        #if !BREEZE_USE_KDE4
+        #if BREEZE_HAVE_KSTYLE
         if( element == CE_CapacityBar )
         {
             fcn = &Style::drawProgressBarControl;
