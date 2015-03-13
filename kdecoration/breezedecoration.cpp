fd3ccba86c472ea89dd7ec35a3e50ac4df36c1b4
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2015-03-13 12:24:31 +0100

    cleanup border calculations

--- kdecoration/breezedecoration.cpp
+++ kdecoration/breezedecoration.cpp
@@ -262,9 +262,6 @@ namespace Breeze
 
         m_internalSettings = SettingsProvider::self()->internalSettings( this );
 
-        // read internal settings
-        // m_internalSettings->read();
-
         // animation
         m_animation->setDuration( m_internalSettings->animationsDuration() );
 
@@ -286,9 +283,13 @@ namespace Breeze
         auto s = settings();
         const auto c = client().data();
         const Qt::Edges edges = c->adjacentScreenEdges();
-        int left   = isMaximizedHorizontally() || edges.testFlag(Qt::LeftEdge) ? 0 : borderSize();
-        int right  = isMaximizedHorizontally() || edges.testFlag(Qt::RightEdge) ? 0 : borderSize();
 
+        // left, right and bottom borders
+        const int left   = isMaximizedHorizontally() || edges.testFlag(Qt::LeftEdge) ? 0 : borderSize();
+        const int right  = isMaximizedHorizontally() || edges.testFlag(Qt::RightEdge) ? 0 : borderSize();
+        const int bottom = isMaximizedVertically() || c->isShaded() || edges.testFlag(Qt::BottomEdge) ? 0 : borderSize(true);
+
+        // top border
         QFontMetrics fm(s->font());
         int top = qMax(fm.boundingRect(c->caption()).height(), buttonHeight() );
 
@@ -300,7 +301,6 @@ namespace Breeze
         // padding above
         top += baseSize*TitleBar_TopMargin;
 
-        int bottom = isMaximizedVertically() || c->isShaded() || edges.testFlag(Qt::BottomEdge) ? 0 : borderSize(true);
         setBorders(QMargins(left, top, right, bottom));
 
         // extended sizes
