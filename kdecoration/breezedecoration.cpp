fc30704cc892bcba2cb474728ed1c3f13e568be8
Kai Uwe Broulik <kde@privat.broulik.de>
2017-12-04 17:06:44 +0100

    [Breeze window decoration] Use intersects instead of contains
    Otherwise breaks if the entire deco is painted at once (e.g. window decoration KCM)

--- kdecoration/breezedecoration.cpp
+++ kdecoration/breezedecoration.cpp
@@ -476,7 +476,7 @@ namespace Breeze
         const auto c = client().data();
         const QRect titleRect(QPoint(0, 0), QSize(size().width(), borderTop()));
 
-        if ( !titleRect.contains(repaintRegion) ) return;
+        if ( !titleRect.intersects(repaintRegion) ) return;
 
         painter->save();
         painter->setPen(Qt::NoPen);
